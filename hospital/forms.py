e73782ca732bfd2816e109e907c18e21c507b166
ADEYA DAVID ODUOR <45120678+AdeyaOduor@users.noreply.github.com>
2022-03-24 00:34:15 +0300

    Update forms.py

--- hospital/forms.py
+++ hospital/forms.py
@@ -14,7 +14,6 @@ class AdminSigupForm(forms.ModelForm):
         }
 
 
-#for student related form
 class DoctorUserForm(forms.ModelForm):
     class Meta:
         model=User
@@ -29,7 +28,6 @@ class DoctorForm(forms.ModelForm):
 
 
 
-#for teacher related form
 class PatientUserForm(forms.ModelForm):
     class Meta:
         model=User
