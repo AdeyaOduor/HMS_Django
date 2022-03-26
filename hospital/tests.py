73ac13e2167f7f868d62f689cf89fc4c64fa6639
ADEYA DAVID ODUOR <45120678+AdeyaOduor@users.noreply.github.com>
2022-03-26 23:31:17 +0300

    Update tests.py

--- hospital/tests.py
+++ hospital/tests.py
@@ -3,21 +3,20 @@ from hospital.models import models.Model
 
 
 class PatientDischargeDetails(models.Model):
-    def patientId(self):
+    def patientDetails(self):
         patientId=models.PositiveIntegerField(null=True)
-    def patientName(self):
         patientName=models.CharField(max_length=40)
-    def assignedDoctorName(self):
         assignedDoctorName=models.CharField(max_length=40)
+        
     def address(self):
         address = models.CharField(max_length=40)
         mobile = models.CharField(max_length=20,null=True)
         symptoms = models.CharField(max_length=100,null=True)
-
+   
+    def patientChargeSheet(self):
         admitDate=models.DateField(null=False)
         releaseDate=models.DateField(null=False)
         daySpent=models.PositiveIntegerField(null=False)
-
         roomCharge=models.PositiveIntegerField(null=False)
         medicineCost=models.PositiveIntegerField(null=False)
         doctorFee=models.PositiveIntegerField(null=False)
