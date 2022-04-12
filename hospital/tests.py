4aaf12b84c896749de44b53dd9e7c0fdf8d93841
ADEYA DAVID ODUOR <45120678+AdeyaOduor@users.noreply.github.com>
2022-04-13 01:26:45 +0300

    Update tests.py

--- hospital/tests.py
+++ hospital/tests.py
@@ -8,25 +8,9 @@ class SignUpFormTest(TestCase):
         expected = ['Firstname','username', 'Lastname', 'password',]
         actual = list(form.fields)
         self.assertSequenceEqual(expected, actual)
-"""""""""
-class PatientDischargeDetails(models.Model):
-    def patientDetails(self):
-        patientId=models.PositiveIntegerField(null=True)
-        patientName=models.CharField(max_length=40)
-        assignedDoctorName=models.CharField(max_length=40)
-        
-    def address(self):
-        address = models.CharField(max_length=40)
-        mobile = models.CharField(max_length=20,null=True)
-        symptoms = models.CharField(max_length=100,null=True)
-   
-    def patientChargeSheet(self):
-        admitDate=models.DateField(null=False)
-        releaseDate=models.DateField(null=False)
-        daySpent=models.PositiveIntegerField(null=False)
-        roomCharge=models.PositiveIntegerField(null=False)
-        medicineCost=models.PositiveIntegerField(null=False)
-        doctorFee=models.PositiveIntegerField(null=False)
-        OtherCharge=models.PositiveIntegerField(null=False)
-        total=models.PositiveIntegerField(null=False)
-""""""""""""""""""""""
+class DoctorSignUpFormTest(TestCase):
+    def test_form_has_fields(self):
+        form = SignUpForm()
+        expected = ['Firstname','username', 'Lastname', 'department','password',]
+        actual = list(form.fields)
+        self.assertSequenceEqual(expected, actual)
