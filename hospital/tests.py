1ae8b1947941233ba4144060b9d4109c7671b4ff
ADEYA DAVID ODUOR <45120678+AdeyaOduor@users.noreply.github.com>
2022-04-12 15:12:47 +0300

    Update tests.py

--- hospital/tests.py
+++ hospital/tests.py
@@ -1,6 +1,13 @@
 from django.test import TestCase
 from hospital.models import models.Model
+from ..forms import SignUpForm
 
+class SignUpFormTest(TestCase):
+    def test_form_has_fields(self):
+        form = SignUpForm()
+        expected = ['username', 'email', 'password1', 'password2',]
+        actual = list(form.fields)
+        self.assertSequenceEqual(expected, actual)
 """""""""
 class PatientDischargeDetails(models.Model):
     def patientDetails(self):
